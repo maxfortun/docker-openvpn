972f8120ce039c76d88c8871b26d3a90105bf6bf
Max <max@maxf.net>
2020-12-12 21:36:01 -0500

    Added

--- docker/bin/mkovpn.sh
+++ docker/bin/mkovpn.sh
@@ -4,12 +4,7 @@ name=client$1
 
 SWD=$(dirname $0)
 
-cd $SWD
-
-# Import our environment variables from systemd
-while read e; do
-	export "$e"
-done < <(tr "\000" "\n" < /proc/1/environ)
+cd /etc/openvpn/client
 
 export OPENVPN_PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com || dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 export OPENVPN_CA=$(cat ca.crt)
@@ -17,4 +12,4 @@ export OPENVPN_CERT=$(cat ${name}.crt)
 export OPENVPN_KEY=$(cat ${name}.key)
 export OPENVPN_TLS_AUTH=$(cat ta.key)
 
-cat client.ovpn.envsubst | envsubst > /etc/openvpn/client/${name}.ovpn
+cat $SWD/client.ovpn.envsubst | envsubst > ${name}.ovpn
--- docker/bin/push_private_routes.sh
+++ docker/bin/push_private_routes.sh
@@ -3,11 +3,6 @@
 SWD=$(dirname $0)
 cd $SWD
 
-tr "\0" "\n" < /proc/1/environ | sed 's/ /\\ /g' > /tmp/env
-set -a
-. /tmp/env
-set +a
-
 for subnet in $OPENVPN_PRIVATE_SUBNETS; do
 	sipcalc $subnet|egrep "Network address\s*-|Network mask\s*-"|cut -d- -f2- |xargs >> /tmp/net
 done
