8988dde455513e0378e162770351c14ab5bc0925
Max Fortun <max@maxf.net>
2020-12-15 23:37:53 -0500

    Fixed routing

--- docker/bin/configure_routing.sh
+++ docker/bin/configure_routing.sh
@@ -3,6 +3,6 @@
 serverIp=$(ip route get 8.8.8.8 | awk '{print $7}')
 vpnIp=$(grep -o '^server [^ ]*' /etc/openvpn/server/server.conf|awk '{ print $2 }')
 
-iptables -t nat -I POSTROUTING -s $vpnIp/24 -o $serverIp -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 
